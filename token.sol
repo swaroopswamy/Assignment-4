c20024a3557fafde83eadde0df38af45b22a1869
swaroopswamy <126262759+swaroopswamy@users.noreply.github.com>
2023-04-04 05:18:58 -0700

    Update token.sol

--- token.sol
+++ token.sol
@@ -1,20 +1,22 @@
-//0xD7ACd2a9FD159E69Bb102A1ca21C9a3e3A5F771B
+//0x55516Ea5315eA573E22C2365Df1ab4a5869eD0dc
 
-pragma solidity ^0.8.0;
+ pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract TokenSale {
+    using SafeMath for uint256;
     address public owner;
-    uint256 public tokensForSale;
-    uint256 public tokensSold;
-    uint256 public price;
+    uint public tokensForSale;
+    uint public tokensSold;
+    uint public price;
     bool public saleActive;
-    mapping(address => uint256) public balances;
+    mapping(address => uint) public balances;
 
-    event SaleStarted(uint256 tokensForSale, uint256 price);
-    event TokensPurchased(address buyer, uint256 tokens);
-    event SaleEnded(uint256 tokensSold);
+    event SaleStarted(uint tokensForSale, uint price);
+    event TokensPurchased(address buyer, uint tokens);
+    event SaleEnded(uint tokensSold);
 
-    constructor(uint256 _tokensForSale, uint256 _price) {
+    constructor(uint _tokensForSale, uint _price) {
         owner = msg.sender;
         tokensForSale = _tokensForSale;
         price = _price;
@@ -22,37 +24,37 @@ contract TokenSale {
     }
 
     function startSale() public {
-        require(msg.sender == owner,"Only the owner can start the sale.");
-        require(!saleActive,"The sale has already started.");
+        require(msg.sender == owner, "Only the owner can start the sale.");
+        require(!saleActive, "The sale has already started.");
 
         saleActive = true;
         emit SaleStarted(tokensForSale, price);
     }
 
-    function buyTokens(uint256 _tokens) public payable {
-        require(saleActive,"The sale is not active.");
-        require(_tokens > 0,"You must purchase at least one token.");
-        require(tokensSold + _tokens <= tokensForSale,"There are not enough tokens left for sale.");
-        require(msg.value == _tokens * price,"You must send enough ether to purchase the requested number of tokens.");
+    function buyTokens(uint _tokens) public payable {
+        require(saleActive, "The sale is not active.");
+        require(_tokens > 0, "You must purchase at least one token.");
+        require(tokensSold .add(_tokens) <= tokensForSale, "There are not enough tokens left for sale.");
+        require(msg.value == _tokens .mul(price), "You must send enough ether to purchase the requested number of tokens.");
 
-        tokensSold += _tokens;
-        balances[msg.sender] += _tokens;
+        tokensSold = tokensSold.add(_tokens);
+        balances[msg.sender] = balances[msg.sender].add(_tokens);
         emit TokensPurchased(msg.sender, _tokens);
     }
 
     function endSale() public {
-        require(msg.sender == owner,"Only the owner can end the sale.");
-        require(saleActive,"The sale is not active.");
+        require(msg.sender == owner, "Only the owner can end the sale.");
+        require(saleActive, "The sale is not active.");
 
         saleActive = false;
         emit SaleEnded(tokensSold);
     }
 
     function withdraw() public {
-        require(balances[msg.sender] > 0,"You have no tokens to withdraw.");
+        require(balances[msg.sender] > 0, "You have no tokens to withdraw.");
 
-        uint256 tokens = balances[msg.sender];
+        uint tokens = balances[msg.sender];
         balances[msg.sender] = 0;
-        payable(msg.sender).transfer(tokens * price);
+        payable(msg.sender).transfer(tokens.mul(price));
     }
 }
